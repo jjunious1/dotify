04c625486c98629fefcb8eba7074b810e8108be2
Justin Junious <justinjunious@gmail.com>
2022-12-08 12:44:08 -0500

    added routes for Auth to server.js

--- dotify_backend/server.js
+++ dotify_backend/server.js
@@ -1,12 +1,17 @@
 const express = require('express')
 const cors = require('cors')
+const AuthRouter = require('./routes/AuthRouter')
 
 const app = express()
 
 const PORT = process.env.PORT || 3001
 
+//middleware
 app.use(cors())
 app.use(express.json())
 app.use(express.urlencoded({ extended: true }))
+
+//routes
+app.use('/auth', AuthRouter)
 
 app.listen(PORT, () => console.log(`Sever Running On Port: ${PORT}`))
