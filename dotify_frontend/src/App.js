f38811c29113739dbfee838e2f0bef029d1c3179
Justin Junious <justinjunious@gmail.com>
2022-12-09 21:09:00 -0500

    added auth useStates and effects to app.js

--- dotify_frontend/src/App.js
+++ dotify_frontend/src/App.js
@@ -1,6 +1,35 @@
 import './App.css'
+import { useState, useEffect } from 'react'
+import { Routes, Route } from 'react'
+import { CheckSession } from './services/Auth'
 
 function App() {
+  const [authenticated, toggleAuthenticated] = useState(false)
+  const [user, setUser] = useState(null)
+
+  const handleLogOut = () => {
+    setUser(null)
+    toggleAuthenticated(false)
+    const token = localStorage.getItem('token')
+    if (token) {
+      localStorage.clear()
+    }
+  }
+
+  const checkToken = async () => {
+    const user = await CheckSession()
+    setUser(user)
+    toggleAuthenticated(true)
+  }
+
+  useEffect(() => {
+    const token = localStorage.getItem('token')
+
+    if (token) {
+      checkToken()
+    }
+  }, [])
+
   return <div></div>
 }
 
