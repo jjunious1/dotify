e5fda561288e06b8bf9a3f80f6466943d8709400
Justin Junious <justinjunious@gmail.com>
2022-12-12 09:30:10 -0500

    added react componnent for audio player and install component

---
+++ dotify_frontend/src/components/MediaPlayer.jsx
@@ -0,0 +1,40 @@
+import AudioPlayer from 'react-h5-audio-player'
+import 'react-h5-audio-player/lib/styles.css'
+import {useState} from 'react'
+
+//referenced this for an audio player
+
+const AudioPlayer = window.ReactH5AudioPlayer.default
+
+const [playlist, setPlaylist]= useState([])
+
+const PlayerApp = () => {
+  const [currentTrack, setTrackIndex] = React.useState(0)
+  const handleClickNext = () => {
+      console.log('click next')
+        setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+    };
+  
+  const handleEnd = () => {
+    console.log('end')
+    setTrackIndex((currentTrack) =>
+            currentTrack < playlist.length - 1 ? currentTrack + 1 : 0
+        );
+  }
+  return (
+      <div className="container">
+        <AudioPlayer
+          volume="0.5"
+          src={playlist[currentTrack].src}
+          showSkipControls
+          onClickNext={handleClickNext}
+          onEnded={handleEnd}
+          // Try other props!
+        />
+      </div>
+    );
+}
+
+export default PlayerApp
